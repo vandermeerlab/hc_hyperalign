72104376fef68c1bf8130b9f3061efa29197c8cd
David Maisson <dmaisson@psych.udel.edu>
2018-08-03 15:21:33 -0400

    basic linspace and chop

--- StdTrialLength.m
+++ StdTrialLength.m
@@ -1,19 +1,24 @@
-function [outputArg1,outputArg2] = SignalChop(metadata,pos)
+function [AdjSig] = SignalChop(TrialStart,TrialEnd,t,signal)
 % This function truncates the signal based on one of two options; chopping
 % the signal based on the start and end point of a traversal through space
 % or by modifying the length of the signal in samples (i.e. time)
 %   Detailed explanation goes here
 %       Inputs
-%           position of the start on cartesian x
-%           position of the start on cartesian y
-%           position of the end on cartesian x
-%           position of the end on cartesian y
+%           trial start
+%           trial end
+%           sampling rate
+%           timestamps
+%           signal
 %       Outputs
-%           signal for a given trial between start and end XYs
-%           adjusted signal of shortest signal to match longest
-%           adjusted signal of longest signal to match shortest
+%           1)signal for a given trial between start and end times
+%           2)adjusted signal of shortest signal to match longest
+%           3)adjusted signal of longest signal to match shortest
 %
-outputArg1 = metadata;
-outputArg2 = pos;
+
+
+t = linspace(t(TrialStart(1,1)),t(TrialEnd(1,end)),length(signal));
+
+AdjSig = signal(t(1,1),t(1,end));
+
 end
 
