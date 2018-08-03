fde9aa77ecb75c625aba7b9bf172eab4dc57573b
David Maisson <dmaisson@psych.udel.edu>
2018-08-02 22:18:26 -0400

    second commit - structure of lenght adjustment script

--- StdTrialLength.m
+++ StdTrialLength.m
@@ -1,7 +1,19 @@
-function [outputArg1,outputArg2] = untitled2(inputArg1,inputArg2)
-%UNTITLED2 Summary of this function goes here
+function [outputArg1,outputArg2] = SignalChop(metadata,pos)
+% This function truncates the signal based on one of two options; chopping
+% the signal based on the start and end point of a traversal through space
+% or by modifying the length of the signal in samples (i.e. time)
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%       Inputs
+%           position of the start on cartesian x
+%           position of the start on cartesian y
+%           position of the end on cartesian x
+%           position of the end on cartesian y
+%       Outputs
+%           signal for a given trial between start and end XYs
+%           adjusted signal of shortest signal to match longest
+%           adjusted signal of longest signal to match shortest
+%
+outputArg1 = metadata;
+outputArg2 = pos;
 end
 
