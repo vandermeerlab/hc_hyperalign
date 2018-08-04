4e8a908d1a449dba77a73486cc96073f223b3c8d
David Maisson <dmaisson@psych.udel.edu>
2018-08-03 22:47:13 -0400

    committing progress

--- SignalChop.m
+++ SignalChop.m
@@ -1,25 +1,34 @@
-function [AdjSig,AdjT] = SignalChop(TrialStart,TrialEnd,t,signal)
-% This function truncates the signal based on one of two options; chopping
-% the signal based on the start and end point of a traversal through space
-% or by modifying the length of the signal in samples (i.e. time)
-%   Detailed explanation goes here
-%       Inputs
-%           trial start
-%           trial end
-%           sampling rate
-%           timestamps
-%           signal
-%       Outputs
-%           1)signal for a given trial between start and end times
-%           2)adjusted signal of shortest signal to match longest
-%           3)adjusted signal of longest signal to match shortest
-%
+function [Trial] = SignalChop(adjust,n,time,TrialStart,TrialEnd)
 
-t_extend = linspace(t(1,1),t(1,end),length(signal));
+%%
+% adjust = 1 for downsample; 2 for upsample; 0 for no change to length
 
-AdjSig(1,:) = signal(t_extend > TrialStart(1,1) & t_extend < TrialEnd(1,1));
-
-AdjT(1,:) = t_extend(t_extend > TrialStart(1,1) & t_extend < TrialEnd(1,1));
-
+%% chop signal from each trial
+for in = 1:n
+    Trial(in).times = time(time > TrialStart & time < TrialEnd);
 end
 
+%% if down sample
+if adjust == 1
+sigleng = length(Trial(1).times);
+for in = 1:length(Trial)
+    if length(Trial(in).times) < sigleng
+        sigleng = length(Trial(in).times);
+    end
+end
+for in = 1:length(Trial)
+    Trial(in).times = linspace(Trial(in).times(1,1),Trial(in).times(1,end),sigleng);
+end
+%% if up sample
+elseif adjust == 2
+sigleng = 0;
+for in = 1:length(Trial)
+    if length(Trial(in).times) > sigleng
+        sigleng = length(Trial(in).times);
+    end
+end
+for in = 1:length(Trial)
+    Trial(in).times = linspace(Trial(in).times(1,1),Trial(in).times(1,end),sigleng);
+end
+end 
+end
