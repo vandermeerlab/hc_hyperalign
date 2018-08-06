eb1285c6db7ad2873578fe952787a88e2e95fbcf
zanewzxie <weizhenxie@Weizhens-MacBook-Pro.local>
2018-08-05 21:06:04 -0400

    organize

--- script.m
+++ script.m
@@ -2,16 +2,16 @@
 % Users/Owners
 % Date of modification
 
-% Add Path 
-addpath('../../mind_2018/hc_hyperalign/SpecFun')
-addpath('/Users/weizhenxie/Documents/Jupyter/mind2018/hc_hyperalign/R042-2013-08-18')
-
+% Add Path
+hc_hyperalign_path = '/Users/weizhenxie/Documents/Jupyter/mind2018';
+addpath([hc_hyperalign_path '/hc_hyperalign/SpecFun'])
+addpath([hc_hyperalign_path '/hc_hyperalign/R042-2013-08-18'])
 
 % load data
-load('R042-2013-08-18-metadata.mat')
+load('R042-2013-08-18-metadata.mat') % metadata
 
 
-% Regularize left and right trials
+%% Regularize left and right trials
 TSE_L(:, 1) = metadata.taskvars.trial_iv_L.tstart;
 TSE_L(:, 2) = metadata.taskvars.trial_iv_L.tend;
 TSE_R(:, 1) = metadata.taskvars.trial_iv_R.tstart;
@@ -19,10 +19,46 @@ TSE_R(:, 2) = metadata.taskvars.trial_iv_R.tend;
 
 reg_trials = RegularizedTrials(TSE_L, TSE_R);
 
-% Produce the Q matrix (Neuron by Time)
-Qmat = generateQmatrix(reg_trials, S, 0);
+%% Produce the Q matrix (Neuron by Time)
+
 Qmat = generateQmatrix(reg_trials, S, 0);
 
-% 
+
+d 
+
+%% PCA on the first trial
+InputMatrix = Qmat.left{1}.Q;
+NumComponents = 3;
+[Egvecs]=pca_egvecs(InputMatrix,NumComponents);
+
+%  project all other trials (both left and right trials) to the same dimension
+for i = 1:size(Qmat.left,2)
+    InputMatrix = Qmat.left{i}.Q;
+    Recon_Qmat.left{i}.Q = pca_project(InputMatrix,Egvecs);
+end
+
+for i = 1:size(Qmat.right,2)
+    InputMatrix = Qmat.right{i}.Q;
+    Recon_Qmat.right{i}.Q = pca_project(InputMatrix,Egvecs);
+end
+
+%% Plot the data 
+
+Qmat = Recon_Qmat;
+
+colors = linspecer(2);
+for i_left = 1:numel(Qmat.left)
+    Q_left = Qmat.left{i_left}.Q;
+    figure(101);plot3(Q_left(:,1), Q_left(:,2), Q_left(:,3), '.','color', colors(1,:));
+    hold on;
+end
+
+for i_right = 1:numel(Qmat.right)
+    Q_right = Qmat.right{i_right}.Q;
+    figure(101);plot3(Q_right(:,1), Q_right(:,2), Q_right(:,3), '.','color', colors(2,:));
+    hold on;
+end
+grid on;
+
 
 
