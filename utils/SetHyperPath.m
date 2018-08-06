cc327a0d6dddbac17b743b7db951d9fa8fdc1ba9
mvdm <mvdm@dartmouth.edu>
2018-08-06 16:15:47 -0400

    Added SetHyperPath() function
    This function resets the MATLAB path to the default, and then adds the `utils` and `SpecFun` folders of the `hc_hyperalign` repo.
    
    Example usage:
      * Before starting work on hc_hyperalign analysis, call this function (either at the top of a script, or using a MATLAB shortcut)
    
    Edit this function to include your machine name and repo path. Note you can include multiple machines.
    
    Future versions of this function could return a `fd` cell array containing paths of data folders. Such an addition would enable analyses to run without having to hard-code data file paths.

---
+++ utils/SetHyperPath.m
@@ -0,0 +1,58 @@
+function SetHyperPath(varargin)
+% function SetHyperPath(varargin)
+%
+% sets path for hc-hyperalign project @ MIND 2018, and returns location of
+% data folders to analyze
+%
+% WARNING: restores default path
+%
+% INPUTS
+%
+% (optional cfg struct fields, values below are defaults)
+%   cfg_def.vandermeerlab = 1; % add vandermeerlab codebase
+%
+% OUTPUTS
+%
+% (none)
+
+cfg.vandermeerlab = 1;
+
+if nargin == 1
+    cfg = varargin{1};
+elseif nargin > 1
+    error('SetHyperPath() requires 0 or 1 input arguments.');
+end    
+restoredefaultpath;
+
+if ispc
+    machinename = getenv('COMPUTERNAME');
+    filesep = '\';
+elseif ismac
+    machinename = getenv('USER');
+    filesep = '/';
+else
+    machinename = getenv('HOSTNAME');
+    filesep = '/';
+end
+
+% get base file path where repo lives
+switch machinename
+    
+    case {'ODYSSEUS'} % add case for your machine
+        base_fp = 'C:\Users\mvdm\Documents\GitHub\hc_hyperalign';
+    case {'mac'}
+        base_fp = '/Users/mac/Projects/hc_hyperalign';
+    case {'NILSDATOR'}
+        base_fp = 'C:\Users\Nils\Desktop\Resa\Git_hyper\hc_hyperalign';
+    case {'Justin-Shins-MacBook-Pro.local'}
+        base_fp = '/Users/justinshin/Desktop/hc_hyperalign';
+
+end
+
+% add to path
+addpath(genpath(cat(2,base_fp,filesep,'SpecFun')));
+addpath(genpath(cat(2,base_fp,filesep,'utils')));
+
+if cfg.vandermeerlab
+   addpath(genpath(cat(2,base_fp,filesep,'vandermeerlab',filesep,'code-matlab',filesep,'shared')));
+end
