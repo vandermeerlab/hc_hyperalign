b2bb1c5a86d1f367880dd18ef6f250a4c5cb1630
justindshin <justinshin@Justin-Shins-MacBook-Pro.local>
2018-08-05 20:51:14 -0400

    fixed some bugs. should work now

--- generateQmatrix.m
+++ generateQmatrix.m
@@ -28,7 +28,8 @@ for l = 1:length(reg_trials.left(:,1));
 end
 
 %get spikes for right trials
-for l = 1:length(reg_trials.right(:,1));
+for l = 1:length(reg_trials.right(:,1))
+    
     for ii = 1:length(spikes.t);
         allspikes = spikes.t(ii)';
         cellspikes = allspikes{1}(allspikes{1}>reg_trials.right(l,1)...
@@ -42,21 +43,33 @@ end
 
 %Get Q Matrices
 for r = 1:length(spikesforQ_left)
-     Q = MakeQfromS([], spikesforQ_left{r})
-     Qmat_data.left{r}.Q = Q.data;
-     Qmat_data.left{r}.time = Q.tvec;
+    if sum(~cellfun(@isempty, spikesforQ_left{r})) > 0
+        Q = MakeQfromS([], spikesforQ_left{r});
+        Qmat_data.left{r}.Q = Q.data;
+        Qmat_data.left{r}.time = Q.tvec;
+    end
 end
+index = cellfun(@isempty, Qmat_data.left) == 0;
+Qmat_data.left = Qmat_data.left(index)
+
 
 for r = 1:length(spikesforQ_right)
-     Q = MakeQfromS([], spikesforQ_right{r})
-     Qmat_data.right{r}.Q = Q.data;
-     Qmat_data.right{r}.time = Q.tvec;
+    if sum(~cellfun(@isempty, spikesforQ_right{r})) > 0
+        Q = MakeQfromS([], spikesforQ_right{r});
+        Qmat_data.right{r}.Q = Q.data;
+        Qmat_data.right{r}.time = Q.tvec;
+    end
 end
+index = cellfun(@isempty, Qmat_data.right) == 0;
+Qmat_data.right = Qmat_data.right(index)
+
+
+
 
 %concatenate everything if you want to
 if concatenate == 1;
     Qmat_data.leftconcat = [];
-    Qmat_data.leftconcat = Qmat_data.left{1}.Q
+    Qmat_data.leftconcat = Qmat_data.left{1}.Q;
     for s = 2:numel(Qmat_data.left)
         Qmat_data.leftconcat = [Qmat_data.leftconcat Qmat_data.left{s}.Q];
     end
