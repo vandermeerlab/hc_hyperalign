c463244ad20149de8e06951ae05c6331b942f9f3
zanewzxie <weizhenxie@Weizhens-MacBook-Pro.local>
2018-08-07 09:41:53 -0400

    revised

--- script_zane.m
+++ script_zane.m
@@ -7,20 +7,16 @@ close all
 
 % Add Path
 hc_hyperalign_path = '/Users/weizhenxie/Documents/Jupyter/mind2018/hc_hyperalign';
-
 addpath([hc_hyperalign_path '/hc_hyperalign/SpecFun'])
 
 
-datatoload = '/R042-2013-08-20/';
-
 % load data
+datatoload = '/R042-2013-08-18/';
 load([hc_hyperalign_path '/Data' datatoload 'metadata.mat']) % metadata
 load([hc_hyperalign_path '/Data' datatoload 'Spikes.mat']) % metadata
 
-
 %% Regularize left and right trials
 wholetrial = 1;
-
 if wholetrial
     TSE_L(:, 1) = metadata.taskvars.trial_iv_L.tstart;
     TSE_L(:, 2) = metadata.taskvars.trial_iv_L.tend;
@@ -31,25 +27,21 @@ else
     load([hc_hyperalign_path '/Data' '/R042-2013-08-18/' 'StemSE.mat']) % metadata
     opt =[];
     reg_trials = RegularizedTrials(StemSE_L, StemSE_R,opt); 
-    
 end
 
-%% Produce the Q matrix (Neuron by Time)
-
-Qmat = generateQmatrix(reg_trials, S, 0);
-
-%%
+%Produce the Q matrix (Neuron by Time)
+Qmat = generateQmatrix(reg_trials, S, 0, 0.05);
 
 % smooth the data 
 for itr = 1:size(Qmat.left,2)
     for it=1:size(Qmat.left{itr}.Q,1)
-      SmoothQ.left{itr}.Q(it,:) = zscore(smoothdata(Qmat.left{itr}.Q(it,:),'gaussian',20));
+      SmoothQ.left{itr}.Q(it,:) = zscore(smoothdata(Qmat.left{itr}.Q(it,:),'gaussian',30));
     end
 end
 
 for itr = 1:size(Qmat.right,2)
     for it=1:size(Qmat.right{itr}.Q,1)
-      SmoothQ.right{itr}.Q(it,:) = zscore(smoothdata(Qmat.right{itr}.Q(it,:),'gaussian',20));
+      SmoothQ.right{itr}.Q(it,:) = zscore(smoothdata(Qmat.right{itr}.Q(it,:),'gaussian',30));
     end
 end
 
@@ -57,12 +49,11 @@ end
 
 %% PCA 
 InputMatrix =[];
-for i = 1:size(Qmat.left,2)
-    InputMatrix=[InputMatrix Qmat.left{i}.Q];
-end
-
 for i = 1:size(Qmat.right,2)
     InputMatrix=[InputMatrix Qmat.right{i}.Q];
+end
+for i = 1:size(Qmat.left,2)
+    InputMatrix=[InputMatrix Qmat.left{i}.Q];
 end
 
 % InputMatrix = SmoothQ.left{1}.Q; 
@@ -75,35 +66,30 @@ for i = 1:size(SmoothQ.left,2)
     InputMatrix = SmoothQ.left{i}.Q;
     Recon_Qmat.left{i}.Q = pca_project(InputMatrix,Egvecs);
 end
-
 for i = 1:size(Qmat.right,2)
     InputMatrix = SmoothQ.right{i}.Q;
     Recon_Qmat.right{i}.Q = pca_project(InputMatrix,Egvecs);
 end
 
-
-
 %% Plot the data 
-
 mat = Recon_Qmat;
 figinx = 101;
 
 colors = linspecer(2);
-
 % need to fix the trial level 
-for i_left = 1:numel(mat.left)
-    Q_left(:,:,i_left) = mat.left{i_left}.Q(1:99,:);
-    figure(figinx);
-    p1=plot3(Q_left(:,1,i_left), Q_left(:,2,i_left), Q_left(:,3,i_left), '-','color',[0 0 1],'LineWidth',3);
+for i = 1: numel(mat.left)
+    Q_left(:,:,i) = mat.left{i}.Q;
+    figure(figinx); 
+    p1=plot3(Q_left(:,1,i), Q_left(:,2,i), Q_left(:,3,i), '-','color',[0 0 1],'LineWidth',3);
     p1.Color(4) = 0.1;
     hold on;
 end
 grid on;
 
-for i_right = 1:numel(mat.right)
-    Q_right(:,:,i_right) = mat.right{i_right}.Q(1:87,:);
+for i = 1:numel(mat.right)
+    Q_right(:,:,i) = mat.right{i}.Q;
     figure(figinx);
-    p1=plot3(Q_right(:,1,i_right), Q_right(:,2,i_right), Q_right(:,3,i_right), '-','color',[1 0 0],'LineWidth',3);
+    p1=plot3(Q_right(:,1,i), Q_right(:,2,i), Q_right(:,3,i), '-','color',[1 0 0],'LineWidth',3);
     p1.Color(4) = 0.1;
     hold on;
 end
@@ -122,7 +108,9 @@ figure(figinx);
 p1=plot3(all_left(:,1), all_left(:,2), all_left(:,3), '-','color',[0 0 1],'LineWidth',3);
 p1.Color(4) = 1;
 xlabel('Component 1');ylabel('Component 2');zlabel('Component 3')
-title('Blue - Left, Red - Right')
+title([datatoload ' : Blue - Left, Red - Right'])
+
+
 
 
 
@@ -133,19 +121,14 @@ title('Blue - Left, Red - Right')
 
 Mats{1}=all_left;
 Mats{2}=all_right;
+Mats{3}=all_left;
+Mats{4}=all_right;
+Mats{5}=all_left;
 
 varargin =Mats;
-for s = 1 %:length(varargin)        
-    if s == 1
-        template = varargin{s};
-    else
-        [~, next] = procrustes((template./(s - 1))', varargin{s}');
-        template = template + next';
-    end
-end
 
 
-
+%step 1: compute common template
 for s = 1:length(varargin)        
     if s == 1
         template = varargin{s};
@@ -155,3 +138,20 @@ for s = 1:length(varargin)
     end
 end
 template = template./length(varargin);
+
+%step 2: align each pattern to the common template template and compute a
+%new common template
+template2 = zeros(size(template));
+for s = 1:length(varargin)
+    [~, next] = procrustes(template', varargin{s}');
+    template2 = template2 + next';
+end
+template2 = template2./length(varargin);
+
+%step 3: align each subject to the mean alignment from the previous round.
+%save the transformation parameters
+[aligned, transforms] = deal(cell(size(varargin)));
+for s = 1:length(varargin)
+    [~, next, transforms{s}] = procrustes(template2', varargin{s}');
+    aligned{s} = next';
+end
