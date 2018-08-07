092e7fb549d2937f21a3dcbd049d83b752215c82
David Maisson <dmaisson@psych.udel.edu>
2018-08-07 12:03:15 -0400

    playing with coherence PCA

--- script_DM.m
+++ script_DM.m
@@ -91,42 +91,94 @@ end
 
 %% Plot the data 
 
-mat = Recon_Qmat;
 figinx = 101;
 
 colors = linspecer(2);
 
 % need to fix the trial level 
-for i_left = 1:numel(mat.left)
-    Q_left(:,:,i_left) = mat.left{i_left}.Q(1:99,:);
+for i = 1:12
+    HC_MEC.Q_Sample(:,:,i) = Sample.HC_MEC.Correct(i).Coh_Recon;
     figure(figinx);
-    p1=plot3(Q_left(:,1,i_left), Q_left(:,2,i_left), Q_left(:,3,i_left), '-','color',[0 0 1],'LineWidth',3);
+    p1=plot3(HC_MEC.Q_Sample(:,1,i), HC_MEC.Q_Sample(:,2,i), HC_MEC.Q_Sample(:,3,i), '-','color',[0 0 1],'LineWidth',3);
     p1.Color(4) = 0.1;
     hold on;
-end
-grid on;
-
-for i_right = 1:numel(mat.right)
-    Q_right(:,:,i_right) = mat.right{i_right}.Q;
+    HC_MEC.Q_Choice(:,:,i) = Choice.HC_MEC.Correct(i).Coh_Recon;
     figure(figinx);
-    p1=plot3(Q_right(:,1,i_right), Q_right(:,2,i_right), Q_right(:,3,i_right), '-','color',[1 0 0],'LineWidth',3);
+    p1=plot3(HC_MEC.Q_Choice(:,1,i), HC_MEC.Q_Choice(:,2,i), HC_MEC.Q_Choice(:,3,i), '-','color',[1 0 0],'LineWidth',3);
+    p1.Color(4) = 0.1;
+    hold on;
+    HC_MEC.Q_Sample_Inc(:,:,i) = Sample.HC_MEC.Incorrect(i).Coh_Recon;
+    figure(figinx);
+    p1=plot3(HC_MEC.Q_Sample_Inc(:,1,i), HC_MEC.Q_Sample_Inc(:,2,i), HC_MEC.Q_Sample_Inc(:,3,i), '-','color',[0 0 1],'LineWidth',3);
+    p1.Color(4) = 0.1;
+    hold on;
+    HC_MEC.Q_Choice_Inc(:,:,i) = Choice.HC_MEC.Incorrect(i).Coh_Recon;
+    figure(figinx);
+    p1=plot3(HC_MEC.Q_Choice_Inc(:,1,i), HC_MEC.Q_Choice_Inc(:,2,i), HC_MEC.Q_Choice_Inc(:,3,i), '-','color',[1 0 0],'LineWidth',3);
+    p1.Color(4) = 0.1;
+    hold on;
+    %
+    HC_RE.Q_Sample(:,:,i) = Sample.HC_Re.Correct(i).Coh_Recon;
+    figure(figinx);
+    p1=plot3(HC_RE.Q_Sample(:,1,i), HC_RE.Q_Sample(:,2,i), HC_RE.Q_Sample(:,3,i), '-','color',[0 0 1],'LineWidth',3);
+    p1.Color(4) = 0.1;
+    hold on;
+    HC_RE.Q_Choice(:,:,i) = Choice.HC_Re.Correct(i).Coh_Recon;
+    figure(figinx);
+    p1=plot3(HC_RE.Q_Choice(:,1,i), HC_RE.Q_Choice(:,2,i), HC_RE.Q_Choice(:,3,i), '-','color',[1 0 0],'LineWidth',3);
+    p1.Color(4) = 0.1;
+    hold on;
+    HC_RE.Q_Sample_Inc(:,:,i) = Sample.HC_Re.Incorrect(i).Coh_Recon;
+    figure(figinx);
+    p1=plot3(HC_RE.Q_Sample_Inc(:,1,i), HC_RE.Q_Sample_Inc(:,2,i), HC_RE.Q_Sample_Inc(:,3,i), '-','color',[0 0 1],'LineWidth',3);
+    p1.Color(4) = 0.1;
+    hold on;
+    HC_RE.Q_Choice_Inc(:,:,i) = Choice.HC_Re.Incorrect(i).Coh_Recon;
+    figure(figinx);
+    p1=plot3(HC_RE.Q_Choice_Inc(:,1,i), HC_RE.Q_Choice_Inc(:,2,i), HC_RE.Q_Choice_Inc(:,3,i), '-','color',[1 0 0],'LineWidth',3);
+    p1.Color(4) = 0.1;
+    hold on;
+    %
+    MEC_RE.Q_Sample(:,:,i) = Sample.MEC_Re.Correct(i).Coh_Recon;
+    figure(figinx);
+    p1=plot3(MEC_RE.Q_Sample(:,1,i), MEC_RE.Q_Sample(:,2,i), MEC_RE.Q_Sample(:,3,i), '-','color',[0 0 1],'LineWidth',3);
+    p1.Color(4) = 0.1;
+    hold on;
+    MEC_RE.Q_Choice(:,:,i) = Choice.MEC_Re.Correct(i).Coh_Recon;
+    figure(figinx);
+    p1=plot3(MEC_RE.Q_Choice(:,1,i), MEC_RE.Q_Choice(:,2,i), MEC_RE.Q_Choice(:,3,i), '-','color',[1 0 0],'LineWidth',3);
+    p1.Color(4) = 0.1;
+    hold on;
+    MEC_RE.Q_Sample_Inc(:,:,i) = Sample.MEC_Re.Incorrect(i).Coh_Recon;
+    figure(figinx);
+    p1=plot3(MEC_RE.Q_Sample_Inc(:,1,i), MEC_RE.Q_Sample_Inc(:,2,i), MEC_RE.Q_Sample_Inc(:,3,i), '-','color',[0 0 1],'LineWidth',3);
+    p1.Color(4) = 0.1;
+    hold on;
+    MEC_RE.Q_Choice_Inc(:,:,i) = Choice.MEC_Re.Incorrect(i).Coh_Recon;
+    figure(figinx);
+    p1=plot3(MEC_RE.Q_Choice_Inc(:,1,i), MEC_RE.Q_Choice_Inc(:,2,i), MEC_RE.Q_Choice_Inc(:,3,i), '-','color',[1 0 0],'LineWidth',3);
     p1.Color(4) = 0.1;
     hold on;
 end
 grid on;
 
 % plot the average
-all_right = mean(Q_right,3);
-figure(figinx);
-p1=plot3(all_right(:,1), all_right(:,2), all_right(:,3), '-','color',[1 0 0],'LineWidth',3);
+HC_MEC.Choice.c = mean(HC_MEC.Q_Choice,3);
+HC_MEC.Sample.c = mean(HC_MEC.Q_Sample,3);
+HC_MEC.Choice.i = mean(HC_MEC.Q_Choice_Inc,3);
+HC_MEC.Sample.i = mean(HC_MEC.Q_Sample_Inc,3);
+
+HC_RE.Choice.c = mean(HC_RE.Q_Choice,3);
+HC_RE.Sample.c = mean(HC_RE.Q_Sample,3);
+HC_RE.Choice.i = mean(HC_RE.Q_Choice_Inc,3);
+HC_RE.Sample.i = mean(HC_RE.Q_Sample_Inc,3);
+
+MEC_RE.Choice.c = mean(MEC_RE.Q_Choice,3);
+MEC_RE.Sample.c = mean(MEC_RE.Q_Sample,3);
+MEC_RE.Choice.i = mean(MEC_RE.Q_Choice_Inc,3);
+MEC_RE.Sample.i = mean(MEC_RE.Q_Sample_Inc,3);
+
+%%
+figure(figinx);hold on;grid on;
+p1=plot3(HC_MEC_Choice(:,1), HC_MEC_Choice(:,2), HC_MEC_Choice(:,3), '-','color',[1 0 0],'LineWidth',3);
 p1.Color(4) = 1;
-mean(Q_right,3)
-
-all_left = mean(Q_left,3);
-figure(figinx);
-p1=plot3(all_left(:,1), all_left(:,2), all_left(:,3), '-','color',[0 0 1],'LineWidth',3);
-p1.Color(4) = 1;
-
-
-
-
