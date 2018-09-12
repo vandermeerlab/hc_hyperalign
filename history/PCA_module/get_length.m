9b8c64c193d4bf47ff2f1543f7c6ee9267d7dfb9
transedward <transedward@gmail.com>
2018-09-12 13:45:01 -0400

    Clean up some files

--- history/PCA_module/get_length.m
+++
@@ -1,5 +0,0 @@
-function datalen = get_length(Q)
-        for iq= 1:size(Q,2)
-         datalen(iq) =size(Q{iq}.data,2);
-        end
-end
--- history/PCA_module/pca_egvecs.m
+++
@@ -1,9 +0,0 @@
-
-function [Egvecs]=pca_egvecs(InputMatrix,NumComponents)
-
-% InputMatrix is matrix N(neurons) by T(time) data
-% output is the Egvecs
-
-[Egvecs] = pca(InputMatrix','NumComponents',NumComponents);
-    
-end
