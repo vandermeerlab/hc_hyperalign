3f07c22207ddcaad3f422bdf81813bf3b0daa775
transedward <transedward@gmail.com>
2018-09-13 17:10:24 -0400

    Further clean-up

--- SpecFun/firstSpike_mind18.m
+++
@@ -1,7 +0,0 @@
-function spk_t = firstSpike(S)
-% function spk_t = firstSpike(S)
-%
-% returns time of first spike
-
-spk = vertcat(S{:});
-spk_t = min(spk);
--- SpecFun/lastSpike_mind18.m
+++
@@ -1,7 +0,0 @@
-function spk_t = lastSpike(S)
-% function spk_t = lastSpike(S)
-%
-% returns time of last spike
-
-spk = vertcat(S{:});
-spk_t = max(spk);
