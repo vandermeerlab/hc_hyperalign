3b16f0fcd0909d905eb05f058b3792f407d73e49
transedward <transedward@gmail.com>
2018-10-18 11:56:09 -0400

    Add out-of-subjects figure

--- scripts/acr_obs.m
+++ scripts/acr_obs.m
@@ -110,13 +110,27 @@ imagesc(zscore_mat);
 colorbar;
 ylabel('Source Sessions');
 xlabel('Target Sessions');
-title('Z-score of distances using source sessions to predict target sessions')
+title('Z-score of distances including within subjects')
 
 imagesc(percent_mat);
 colorbar;
 ylabel('Source Sessions');
 xlabel('Target Sessions');
-title('Percentile of distances using source sessions to predict target sessions')
+title('Percentile of distances including within subjects')
+
+out_zscore_mat = set_withsubj_nan(zscore_mat);
+imagesc(out_zscore_mat,'AlphaData', ~isnan(out_zscore_mat));
+colorbar;
+ylabel('Source Sessions');
+xlabel('Target Sessions');
+title('Z-score of distances excluding within subjects')
+
+out_percent_mat = set_withsubj_nan(percent_mat);
+imagesc(out_percent_mat,'AlphaData', ~isnan(out_percent_mat));
+colorbar;
+ylabel('Source Sessions');
+xlabel('Target Sessions');
+title('Percentile of distances excluding within subjects')
 
 % % Plot shuffle distance histogram and true distance (by shuffling Q matrix)
 % for i = 1:length(Q)
