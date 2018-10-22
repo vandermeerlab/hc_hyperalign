90d3e97e707747403c1708e0767eeb94763080db
transedward <transedward@gmail.com>
2018-10-22 19:00:59 -0400

    Minor refactoring

--- utils/get_aligned_left_right.m
+++ utils/get_aligned_left_right.m
@@ -1,10 +1,10 @@
-function [aligned_left, aligned_right] = get_aligned_left_right(mean_proj)
+function [aligned_left, aligned_right] = get_aligned_left_right(pre_aligned)
     % Hyperalignment
-    for i = 1:length(mean_proj.left)
-        hyper_input{i} = [mean_proj.left{i}, mean_proj.right{i}];
+    for i = 1:length(pre_aligned.left)
+        hyper_input{i} = [pre_aligned.left{i}, pre_aligned.right{i}];
     end
     [aligned, transforms] = hyperalign(hyper_input{:});
-    w_len = size(mean_proj.left{1}, 2);
+    w_len = size(pre_aligned.left{1}, 2);
     aligned_left = cellfun(@(x) x(:, 1:w_len), aligned, 'UniformOutput', false);
     aligned_right = cellfun(@(x) x(:, w_len+1:end), aligned, 'UniformOutput', false);
 end
