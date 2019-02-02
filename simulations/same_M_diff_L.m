cf0344b3ce4e4132b9263ea6bee131077e2c33ce
transedward <transedward@gmail.com>
2019-02-02 17:33:31 -0500

    Use real M in same_M_diff_L

--- simulations/same_M_diff_L.m
+++ simulations/same_M_diff_L.m
@@ -24,15 +24,25 @@ end
 % Perform hyperalignment on concatenated [L, R] in PCA.
 hyper_input = {proj_Q{1}, proj_Q{2}};
 [aligned_left, aligned_right, transforms] = get_aligned_left_right(hyper_input);
-% Only for this for dimensions of M.
-[~, ~, M] = procrustes(aligned_right{1}', aligned_left{1}');
-rand_M.c = zeros(size(M.c));
-% 1 if we don't want scaling.
-rand_M.b = 1;
-rand_M.T = randn(size(M.T));
+
+% Use M from real data
+% Get real Q inputs.
+cfg_data = [];
+real_Q = prepare_all_Q(cfg_data);
+% PCA
+NumComponents = 10;
+for rq_i = 1:length(real_Q)
+    r_proj_Q{rq_i} = perform_pca(real_Q{rq_i}, NumComponents);
+end
+
+% Hyperalignment
+[r_aligned_left, r_aligned_right] = get_aligned_left_right({r_proj_Q{1}, r_proj_Q{19}});
+[~, ~, M] = procrustes(r_aligned_right{1}', r_aligned_left{1}');
+% Disable scaling
+M.b = 1;
 
 for q_i = 1:length(Q)
-    predicted{q_i} = p_transform(rand_M, aligned_left{q_i});
+    predicted{q_i} = p_transform(M, aligned_left{q_i});
     project_back_pca{q_i} = inv_p_transform(transforms{q_i}, [aligned_left{q_i}, predicted{q_i}]);
     Q{q_i}.right = eigvecs{q_i} * project_back_pca{q_i}(:, w_len+1:end);
 end
