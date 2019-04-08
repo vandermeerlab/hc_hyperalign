730c8cd20ccf24d3c8ffc1c7f21c2d0a62714443
transedward <transedward@gmail.com>
2019-04-08 15:51:34 -0400

    Add .fig in save_all_figs

--- utils/save_all_figs.m
+++ utils/save_all_figs.m
@@ -1,4 +1,5 @@
 function save_all_figs(save_dir, fname)
     save_as_eps(save_dir, fname);
     print(gcf, '-dpng', '-r300', cat(2, save_dir, '/', fname, '.png'));
+    saveas(gcf, cat(2, save_dir, '/', fname, '.fig'), 'fig');
 end
