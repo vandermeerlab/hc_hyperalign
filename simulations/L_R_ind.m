b775ab2884e020a845afdbfbe5c825809d528020
transedward <transedward@gmail.com>
2019-06-21 15:45:45 -0400

    Make L_xor_R and L_R_ind functions

--- simulations/L_R_ind.m
+++ simulations/L_R_ind.m
@@ -1,48 +1,40 @@
-%% Prepare input
-% Last 2.4 second, dt = 50ms
-w_len = 48;
-% Or last 41 bins (after all choice points) for TC
-% w_len = 41;
-rng(mean('hyperalignment'));
-% Make two Qs - first: source, second: target
-for q_i = 1:19
-    % Number of neurons
-    n_units = randi([60, 120]);
-    Q{q_i}.left = zeros(n_units, w_len);
-    Q{q_i}.right = zeros(n_units, w_len);
-    p_has_field = 0.5;
-    p_has_noise = 0.3;
-    for n_i = 1:n_units
-        % mu = rand() * w_len;
-        % peak = rand() * 0.5 + 0.5;
-        % sig = rand() * 5 + 2;
-        if rand() < p_has_field
-            left_mu = rand() * w_len;
-            left_peak = rand() * 0.5 + 0.5;
-            left_sig = rand() * 5 + 2;
-            Q{q_i}.left(n_i, :) = gaussian_1d(w_len, left_peak, left_mu, left_sig);
-        end
-        if rand() < p_has_field
-            right_mu = rand() * w_len;
-            right_peak = rand() * 0.5 + 0.5;
-            right_sig = rand() * 5 + 2;
-            Q{q_i}.right(n_i, :) = gaussian_1d(w_len, right_peak, right_mu, right_sig);
-        end
-        if rand() < p_has_noise
-            Q{q_i}.left(n_i, :) = Q{q_i}.left(n_i, :) + 0.001 * rand(size(Q{q_i}.left(n_i, :)));
-            Q{q_i}.right(n_i, :) = Q{q_i}.right(n_i, :) + 0.001 * rand(size(Q{q_i}.right(n_i, :)));
+function [Q] = L_R_ind(cfg_in)
+    % Last 2.4 second, dt = 50ms, or last 41 bins (after all choice points) for TC
+    cfg_def.w_len = 48;
+    cfg_def.same_mu = 0;
+
+    mfun = mfilename;
+    cfg = ProcessConfig(cfg_def,cfg_in,mfun);
+
+    rng(mean('hyperalignment'));
+    for q_i = 1:19
+        % Number of neurons
+        n_units = randi([60, 120]);
+        Q{q_i}.left = zeros(n_units, cfg.w_len);
+        Q{q_i}.right = zeros(n_units, cfg.w_len);
+        p_has_field = 0.5;
+        for n_i = 1:n_units
+            mu = rand() * cfg.w_len;
+            if rand() < p_has_field
+                if cfg.same_mu
+                    left_mu = mu;
+                else
+                    left_mu = rand() * cfg.w_len;
+                end
+                left_peak = rand() * 0.5 + 0.5;
+                left_sig = rand() * 5 + 2;
+                Q{q_i}.left(n_i, :) = gaussian_1d(cfg.w_len, left_peak, left_mu, left_sig);
+            end
+            if rand() < p_has_field
+                if cfg.same_mu
+                    right_mu = mu;
+                else
+                    right_mu = rand() * cfg.w_len;
+                end
+                right_peak = rand() * 0.5 + 0.5;
+                right_sig = rand() * 5 + 2;
+                Q{q_i}.right(n_i, :) = gaussian_1d(cfg.w_len, right_peak, right_mu, right_sig);
+            end
         end
     end
-
-%     cfg_data.removeCorrelations = 'pos_and_neg';
-%     cfg_data.removeNaNFiring = true;
-%     Q = remove_corr_cells(cfg_data, Q);
-
-    % Different normalization.
-    % Ind. normalization
-    Q_norm_ind{q_i} = normalize_Q('ind', Q{q_i});
-    % Concat. normalization
-    Q_norm_concat{q_i} = normalize_Q('concat', Q{q_i});
-    % Subtract the means
-    Q_norm_sub{q_i} = normalize_Q('sub_mean', Q{q_i});
 end
--- simulations/L_xor_R.m
+++ simulations/L_xor_R.m
@@ -1,31 +1,27 @@
-%% Prepare input
-% Last 2.4 second, dt = 50ms
-w_len = 48;
-% Or last 41 bins (after all choice points) for TC
-% w_len = 41;
-rng(mean('hyperalignment'));
-% Make two Qs - first: source, second: target
-for q_i = 1:19
-    % Number of neurons
-    n_units = randi([60, 120]);
-    Q{q_i}.left = zeros(n_units, w_len);
-    Q{q_i}.right = zeros(n_units, w_len);
-    for n_i = 1:n_units
-        mu = rand() * w_len;
-        peak = rand() * 0.5 + 0.5;
-        sig = rand() * 5 + 2;
-        left_has_field = rand() < 0.5;
-        if left_has_field
-            Q{q_i}.left(n_i, :) = gaussian_1d(w_len, peak, mu, sig);
-        else
-            Q{q_i}.right(n_i, :) = gaussian_1d(w_len, peak, mu, sig);
+function [Q] = L_xor_R(cfg_in)
+    % Last 2.4 second, dt = 50ms, or last 41 bins (after all choice points) for TC
+    cfg_def.w_len = 48;
+
+    mfun = mfilename;
+    cfg = ProcessConfig(cfg_def,cfg_in,mfun);
+
+    rng(mean('hyperalignment'));
+    % Make two Qs - first: source, second: target
+    for q_i = 1:19
+        % Number of neurons
+        n_units = randi([60, 120]);
+        Q{q_i}.left = zeros(n_units, cfg.w_len);
+        Q{q_i}.right = zeros(n_units, cfg.w_len);
+        for n_i = 1:n_units
+            mu = rand() * cfg.w_len;
+            peak = rand() * 0.5 + 0.5;
+            sig = rand() * 5 + 2;
+            left_has_field = rand() < 0.5;
+            if left_has_field
+                Q{q_i}.left(n_i, :) = gaussian_1d(cfg.w_len, peak, mu, sig);
+            else
+                Q{q_i}.right(n_i, :) = gaussian_1d(cfg.w_len, peak, mu, sig);
+            end
         end
     end
-    % Different normalization.
-    % Ind. normalization
-    Q_norm_ind{q_i} = normalize_Q('ind', Q{q_i});
-    % Concat. normalization
-    Q_norm_concat{q_i} = normalize_Q('concat', Q{q_i});
-    % Subtract the means
-    Q_norm_sub{q_i} = normalize_Q('sub_mean', Q{q_i});
 end
