1190098421d0361ddc845dc94b4e8797507aa6a5
Hung-Tu Chen <transedward@gmail.com>
2019-06-27 15:16:55 -0400

    Add a few things about testing # of shuffles

--- sandboxs/sandbox_num_of_shuffles.m
+++ sandboxs/sandbox_num_of_shuffles.m
@@ -19,7 +19,7 @@ mean_p_vals = zeros(1, length(num_shuffles));
 std_p_vals = zeros(1, length(num_shuffles));
 
 for s_i = 1:length(num_shuffles)
-    p_vals = zeros(1, 10);
+    p_vals = zeros(1, 30);
     for p_i = 1:length(p_vals)
         % Shuffling operations
         n_shuffles = num_shuffles(s_i);
@@ -31,12 +31,17 @@ for s_i = 1:length(num_shuffles)
             sf_dists_mat(:, :, i) = s_actual_dists_mat;
         end
         cfg.use_adr_data = 0;
-        % Matrix of differences between actual distance (identity distance) and mean of shuffled distance.
-        actual_mean_sf = actual_dists_mat - mean(sf_dists_mat, 3);
-        out_actual_mean_sf = set_withsubj_nan(cfg, actual_mean_sf);
-        % Binomial stats
-        bino_p_mean = calculate_bino_p(sum(sum(out_actual_mean_sf < 0)), sum(sum(~isnan(out_actual_mean_sf))), 0.5);
-        p_vals(p_i) = bino_p_mean;
+        zscore_mat = zeros(length(data));
+        for i = 1:length(data)
+            for j = 1:length(data)
+                sf_dists = squeeze(sf_dists_mat(i, j, :))';
+                zs = zscore([sf_dists, actual_dists_mat(i, j)]);
+                zscore_mat(i, j) = zs(end);
+            end
+        end
+        out_zscore_mat = set_withsubj_nan(cfg, zscore_mat);
+        signrank_p = signrank(out_zscore_mat(:));
+        p_vals(p_i) = signrank_p;
     end
     mean_p_vals(s_i) = mean(p_vals);
     std_p_vals(s_i) = std(p_vals);
