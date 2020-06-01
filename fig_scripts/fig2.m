fc6e9559448a8a8fa63eb4d0edbc74eb8045a7bd
transedward <transedward@gmail.com>
2020-06-01 15:27:12 -0400

    Remove pca vs. shuffles in fig2 (Temporarily)

--- fig_scripts/fig2.m
+++ fig_scripts/fig2.m
@@ -7,7 +7,8 @@ datas = {Q, adr_Q};
 for d_i = 1:length(datas)
     data = datas{d_i};
     [actual_dists_mat{d_i}, id_dists_mat{d_i}, sf_dists_mat{d_i}] = predict_with_shuffles([], data, @predict_with_L_R);
-    [actual_dists_mat_pca{d_i}, id_dists_mat_pca{d_i}, sf_dists_mat_pca{d_i}] = predict_with_shuffles([], data, @predict_with_L_R_pca);
+    [actual_dists_mat_pca{d_i}, id_dists_mat_pca{d_i}] = predict_with_L_R_pca([], data);
+%     [actual_dists_mat_pca{d_i}, id_dists_mat_pca{d_i}, sf_dists_mat_pca{d_i}] = predict_with_shuffles([], data, @predict_with_L_R_pca);
 end
 
 %% Source-target figures in Carey
@@ -28,15 +29,14 @@ for m_i = 1:length(matrix_obj)
     cfg_plot.title = titles{m_i};
 
     plot_matrix(cfg_plot, matrix_obj{m_i});
-
 end
 set(gcf, 'Position', [316 185 898 721]);
 
 %% Hypertransform and PCA-only in Carey and ADR
-x_limits = {[-6.5, 6.5], [-1.05e5, 1.05e5], [0, 1]}; % two rows, three columns in figure
-x_tick = {-6:6, -1e5:2.5e4:1e5, 0:0.2:1};
+x_limits = {[-6.5, 6.5], [-2.05e4, 2.05e4], [0, 1]}; % two rows, three columns in figure
+x_tick = {-6:6, -2e4:2.5e3:2e4, 0:0.2:1};
 xtick_labels = {{-6, 6}, {sprintf('-1\\times10^{%d}', 5), sprintf('1\\times10^{%d}', 5)}, {0, 1}};
-binsizes = [1, 1.5e4, 0.1]; % for histograms
+binsizes = [1, 3.5e3, 0.1]; % for histograms
 
 cfg_plot = [];
 cfg_plot.hist_colors = {colors.HT.hist, colors.pca.hist};
@@ -50,18 +50,18 @@ for d_i = 1:length(datas) % one row each for Carey, ADR
     end
     [z_score{d_i}, mean_shuffles{d_i}, proportion{d_i}] = calculate_common_metrics(cfg_metric, actual_dists_mat{d_i}, ...
         id_dists_mat{d_i}, sf_dists_mat{d_i});
-    [z_score_pca{d_i}, mean_shuffles_pca{d_i}, proportion_pca{d_i}] = calculate_common_metrics(cfg_metric, actual_dists_mat_pca{d_i}, ...
-        id_dists_mat_pca{d_i}, sf_dists_mat{d_i});
+%     [z_score_pca{d_i}, mean_shuffles_pca{d_i}, proportion_pca{d_i}] = calculate_common_metrics(cfg_metric, actual_dists_mat_pca{d_i}, ...
+%         id_dists_mat_pca{d_i}, sf_dists_mat{d_i});
     
     % Hard to deal with value on the limit, ex: a lot of ones in proportion
     % Workaround: Make them into 0.9999, only for visualization purpose
     keep_idx = ~isnan(proportion{d_i}.out_actual_sf_mat);
     proportion_mat = min(proportion{d_i}.out_actual_sf_mat(keep_idx), 0.9999);
-    proportion_mat_pca = min(proportion_pca{d_i}.out_actual_sf_mat(keep_idx), 0.9999);
+%     proportion_mat_pca = min(proportion_pca{d_i}.out_actual_sf_mat(keep_idx), 0.9999);
 
-    matrix_objs = {{z_score{d_i}.out_zscore_mat, z_score_pca{d_i}.out_zscore_mat}, ...
-        {mean_shuffles{d_i}.out_actual_mean_sf, mean_shuffles_pca{d_i}.out_actual_mean_sf}, ...
-        {proportion_mat, proportion_mat_pca}};
+    matrix_objs = {{z_score{d_i}.out_zscore_mat}, ...
+        {mean_shuffles{d_i}.out_actual_mean_sf}, ...
+        {proportion_mat}};
 
     for m_i = 1:length(matrix_objs) % loop over columns
         this_ax = subplot(3, 3, (3 * d_i) + m_i);
