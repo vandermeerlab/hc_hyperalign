0575605d40e2facd025d911fedfddfe27475e07b
transedward <transedward@gmail.com>
2020-07-03 12:30:39 -0400

    Add exanple row plotting of supp_fig 6 (might be useful in the future)

--- fig_scripts/supp_fig6.m
+++ fig_scripts/supp_fig6.m
@@ -83,3 +83,43 @@ for d_i = 1:length(datas)
     xlabel('L');
     ylabel('R');
 end
+
+%% Example row
+colors = get_hyper_colors();
+
+sess_idx = 5;
+ex_cell_idx = 52;
+datas = {Q{sess_idx}.left(ex_cell_idx, :), Q{sess_idx}.right(ex_cell_idx, :)};
+predictions = {out_predicted_Q_mat{6, sess_idx}(ex_cell_idx, w_len+1:end), ...
+        out_predicted_Q_mat{9, sess_idx}(ex_cell_idx, w_len+1:end), ...
+        out_predicted_Q_mat{15, sess_idx}(ex_cell_idx, w_len+1:end)};
+
+fit_colors = {colors.HT.fit, colors.pca.fit, colors.wh.fit, colors.ID.fit, [198/255 113/255 113/255]};
+
+cfg_plot.fs = 24;
+cfg_plot.xlim = [0, 48];
+cfg_plot.binsize = 2;
+cfg_plot.xtick = 0:12:48;
+cfg_plot.xtick_label = {'start', 'end'};
+cfg_plot.fit = 'kernel';
+
+for i = 1:2
+    cfg_plot.fit_colors = {fit_colors{i}};
+    cfg_plot.ax = subplot(1, 2, i);
+    plot_hist2(cfg_plot, {datas{i}});
+    xlabel('time'); ylabel('firing rate');
+    ylim([0, 20]);
+    if i == 1
+        title('left')
+    else
+        title('right')
+        hold on;
+    end
+end
+
+for j = 1:3
+    cfg_plot.fit_colors = {fit_colors{2+j}};
+    cfg_plot.ax = subplot(1, 2, 2);
+    hold on;
+    plot_hist2(cfg_plot, {predictions{j}});
+end
