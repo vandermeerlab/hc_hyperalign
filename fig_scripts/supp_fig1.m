2220d812d4e4f0e2d3c9eedd728726a88c1b4274
transedward <transedward@gmail.com>
2021-02-11 11:58:26 -0800

    Modify supp_fig1 example

--- fig_scripts/supp_fig1.m
+++ fig_scripts/supp_fig1.m
@@ -1,10 +1,36 @@
 rng(mean('hyperalignment'));
 A = [2 0 0; 0 2 0; 1 0 0; 0 1 0; 0 0 1; 1 0 0];
-B = [0 0 2; 2 0 0; 0 1 0; 0 0 1; 0 0 1; 1 0 0];
+low_shift_idx_A = randsample(3:6, 2)
+B = [0 0 2; 2 0 0; 0 1 0; 0 1 0; 0 0 1; 0 1 0];
 
-shuffle_idx = randperm(size(A, 1));
-C = A(shuffle_idx, :);
-D = B(shuffle_idx, :);
+% Initialized subject 2 Left randomly
+C = zeros(size(A));
+for i = 1:length(A)
+    prob = rand();
+    if prob < 1/3
+        C(i, :) = [1 0 0];
+    elseif prob < 2/3
+        C(i, :) = [0 1 0];
+    else
+        C(i, :) = [0 0 1];
+    end
+end
+
+% Make the first two rows high-firing
+C(1:2, :) = C(1:2, :) * 2;
+% Randomly choose two low-firing-rate cells to shift left in D
+low_shift_idx = randsample(3:6, 2);
+
+D = C;
+% High-firing-rate cells shift left, chosen low-firing ones shift right
+D(1, :) = circshift(D(1, :), -1);
+D(2, :) = circshift(D(2, :), -1);
+D(low_shift_idx(1), :) = circshift(D(low_shift_idx(1), :), 1);
+D(low_shift_idx(2), :) = circshift(D(low_shift_idx(2), :), 1);
+
+shuffle_idx = randperm(size(C, 1));
+C = C(shuffle_idx, :);
+D = D(shuffle_idx, :);
 
 Q{1}.left = A; Q{1}.right = B;
 Q{2}.left = C; Q{2}.right = D;
@@ -65,5 +91,4 @@ title('Predicted R');
 %% cell-by-cell and PV correlations
 cell_coefs = calculate_cell_coefs(Q);
 PV_coefs = calculate_PV_coefs(Q);
-plot_PV([], PV_coefs);
 off_diag_PV_coef = get_off_dig_PV(PV_coefs);
