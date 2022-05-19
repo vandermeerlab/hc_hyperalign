a80d132d3c56df71b98cafb053ddd1a2d48b2bca
transedward <transedward@gmail.com>
2022-05-19 14:10:41 -0400

    Aligning sorted place fields

--- null_models/gaussian_procrustes.m
+++ null_models/gaussian_procrustes.m
@@ -1,10 +1,10 @@
 %% Two conditions of 1d gaussian tuning curves with fixed sigma and number of neurons
 w_len = 50;
-n_units = 50;
+n_units = 100;
 X = zeros(n_units, w_len);
 Y = zeros(n_units, w_len);
 
-p_has_field = 0.5;
+p_has_field = 1;
 
 for n_i = 1:n_units
     mu_1 = rand() * w_len;
@@ -19,6 +19,15 @@ for n_i = 1:n_units
     end
 end
 
+[~, max_idx_X] = max(X, [], 2);
+[~, sorted_idx_X] = sort(max_idx_X);
+
+[~, max_idx_Y] = max(Y, [], 2);
+[~, sorted_idx_Y] = sort(max_idx_Y);
+
+X = X(sorted_idx_X, :);
+Y = Y(sorted_idx_Y, :);
+
 figure;
 subplot(1, 3, 1); imagesc(X); colorbar;
 xlabel('locations'); ylabel('neurons'); title('X');
@@ -31,6 +40,13 @@ set(gca, 'FontSize', 18);
 %% (Hyper/procrustes)-aligen two inputs
 [d, Z, transform] = procrustes(X, Y, 'scaling', false);
 RMSE_neuron_avg = mean(sqrt(mean((X - Z).^2, 2)));
+
+% Calculate the Frobenius norm of the prediction error matrix
+% Fro_error = sum((X - Z).^2, 'all');
+% % Normalize by the average of Frobenius norms of X and Y
+% norm_factor = (sum(X.^2, 'all') + sum(Y.^2, 'all')) / 2;
+% R_sq = 1 - (Fro_error / norm_factor);
+
 subplot(1, 3, 3); imagesc(Z); colorbar;
 xlabel('locations'); ylabel('neurons'); title('Prediction of X');
 set(gca, 'FontSize', 18);
@@ -40,14 +56,21 @@ set(gca, 'FontSize', 18);
 w_len = 50;
 n_units_list = 50:5:150;
 sigmas_list = 1:0.5:12.5;
-p_has_field = 0.5;
+p_has_field = 1;
 
-RMSE_mat = zeros(length(n_units_list), length(sigmas_list));
+sim_measure_mat = zeros(length(n_units_list), length(sigmas_list));
 
 for u_i = 1:length(n_units_list)
     n_units = n_units_list(u_i);
     X = zeros(n_units, w_len);
     Y = zeros(n_units, w_len);
+    
+%     [~, max_idx] = max(Y, [], 2);
+%     [~, sorted_idx] = sort(max_idx);
+    
+%     X = X(sorted_idx, :);
+%     Y = Y(sorted_idx, :);
+
     for s_i = 1:length(sigmas_list)
         sigma = sigmas_list(s_i);
         for n_i = 1:n_units
@@ -62,13 +85,24 @@ for u_i = 1:length(n_units_list)
             end
         end
         [d, Z, transform] = procrustes(X, Y, 'scaling', false);
+        
         RMSE_neuron_avg = mean(sqrt(mean((X - Z).^2, 2)));
-        RMSE_mat(u_i, s_i) = RMSE_neuron_avg;
+        sim_measure_mat(u_i, s_i) = RMSE_neuron_avg;
+        
+%         % Calculate the Frobenius norm of the prediction error matrix
+%         Fro_error = sum((X - Z).^2, 'all');
+%         % Normalize by the average of Frobenius norms of X and Y
+%         norm_factor = (sum(X.^2, 'all') + sum(Y.^2, 'all')) / 2;
+%         R_sq = 1 - (Fro_error / norm_factor);
+%         
+%         sim_measure_mat(u_i, s_i) = R_sq;
     end
 end
 
-figure; imagesc(RMSE_mat); colorbar;
+figure; imagesc(sim_measure_mat); colorbar;
 xlabel('sigma'); ylabel('# of neurons');
 xticks(1:2:length(sigmas_list)); xticklabels(1:12);
 yticks(1:2:length(n_units_list)); yticklabels(50:10:150);
-set(gca, 'FontSize', 18)
+set(gca, 'FontSize', 18);
+title('RMSE per neuron when p(has field)= 1');
+% title('R^2 when p(has field)= 0.5');
